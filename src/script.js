36874a1fe4e8c231d6a32d85fa7c140e8d501acb
JuliaSmyshnikova <smyshnikova1@gmail.com>
2022-12-28 17:06:10 +0100

    update js

--- src/script.js
+++ src/script.js
@@ -15,13 +15,15 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayWeather(response) {
+function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.maim.temp);
   cityElement.innerHTML = response.date.name;
@@ -29,10 +31,14 @@ function displayWeather(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "scr",
+    `http://openweathermap.org/img/wn/04d@2x.png`
+  );
 }
 
 let apiKey = "7d0fd8c6293b573801711c68993c7ff2";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric` ;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayWeather);
+axios.get(apiUrl).then(displayTemperature);

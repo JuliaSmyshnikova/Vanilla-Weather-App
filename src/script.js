6e42411373f4e5c8552869f1fd3ce525744c564e
JuliaSmyshnikova <smyshnikova1@gmail.com>
2023-01-03 12:24:12 +0100

    Debugging /change colour

--- src/script.js
+++ src/script.js
@@ -17,10 +17,10 @@ function formatDate(timestamp) {
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let day = date.getDate();
+  let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  return day[days];
+  return days[day];
 }
 
 function displayForecast(response) {
